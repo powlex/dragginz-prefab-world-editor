091e90a3a531002a27da1b27a3c23fc3980e828d
obrodhage <obrodhage@gmail.com>
2018-08-14 13:35:53 -0700

    added level chunk info popup

---
+++ Assets/_PrefabWorldEditor/Scripts/UI/PweLevelInfo.cs
@@ -0,0 +1,47 @@
+﻿//
+// Author  : Oliver Brodhage
+// Company : Decentralised Team of Developers
+//
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+using AssetsShared;
+
+namespace PrefabWorldEditor
+{
+	public class PweLevelInfo : MonoSingleton<PweLevelInfo>
+    {
+		public Transform infoPanel;
+
+        public Text levelName;
+        public Text levelSize;
+        public Text levelPos;
+        public Text levelUpdated;
+
+        #region SystemMethods
+
+        void Awake () {
+
+			showInfoPanel (false);
+        }
+
+		#endregion
+
+		#region PublicMethods
+
+		public void showInfoPanel(bool state) {
+
+            if (infoPanel != null) {
+                infoPanel.gameObject.SetActive(state);
+            }
+		}
+
+        #endregion
+
+        #region PrivateMethods
+
+        #endregion
+    }
+}
